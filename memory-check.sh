ffe5ff9cb5fff06c2d9fe92254f461a11c48318e
austinsenv <austins.systems@protonmail.com>
2026-08-17 21:11:49 -0500

    add config.env and initial completion of memory-check.sh

--- memory-check.sh
+++ memory-check.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# source the configuration
+source config.env
+
 # get the latest memory usage
 MEMTOTAL=$(grep "MemTotal" /proc/meminfo | awk '{print $2}')
-MEMFREE=$(grep "MemFree" /proc/meminfo | awk '{print $2}')
 MEMAVAILABLE=$(grep "MemAvailable" /proc/meminfo | awk '{print $2}')
 
+# determine memory utilization percentage
+PERCENT_AVAILABLE=$(awk "BEGIN {printf \"%.2f\", ($MEMAVAILABLE/$MEMTOTAL)*100}")
+
+# determine if an alert should be sent
+if awk "BEGIN {exit !($PERCENT_AVAILABLE >= 100 - ${MEM_TRIGGER%\%})}"; then
+  echo "HEALTHY"
+  echo "-------"
+else
+  echo "UNHEALTHY"
+  echo "---------"
+fi
 
 echo "MemTotal: $MEMTOTAL"
 echo "MemFree: $MEMFREE"
 echo "MemAvailable: $MEMAVAILABLE"
-
+echo "Percentage Available: $PERCENT_AVAILABLE"
 
